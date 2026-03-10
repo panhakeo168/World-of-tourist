a53c879f4a6b1097731e2ca6bfd4b4a14f81f2b2
panhakeo168 <panhakeo168@gmail.com>
2026-03-10 15:46:05 +0700

    first commit

---
+++ source/TuristPlaces/data.js
@@ -0,0 +1,10 @@
+export const userdata = [
+  { id: 1, name: "panha", age: 26 },
+  { id: 1, name: "panha", age: 26 },
+  { id: 1, name: "panha", age: 26 },
+  { id: 1, name: "panha", age: 26 },
+  { id: 1, name: "panha", age: 26 },
+  { id: 1, name: "panha", age: 25 },
+];
+
+// name export,and deafault export for default export in one file of module you can export only for one file
